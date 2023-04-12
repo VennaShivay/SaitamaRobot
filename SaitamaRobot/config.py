163555c3d5b7ff40d8e06c622888330dfb7faa8f
VennaShiva <125667228+VennaShivay@users.noreply.github.com>
2023-04-12 18:28:31 +0530

    Update config.py

--- SaitamaRobot/config.py
+++ SaitamaRobot/config.py
@@ -5,7 +5,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/AstrakoBot/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/SaitamaRobot/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
